a77b858826bd058369c153f9661a909e56b62b67
AHronec7 <austinhronec16@yahoo.com>
2018-09-27 17:22:28 -0400

    Dataset uploaded

---
+++ DatasetPractice/DatasetPractice/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace DatasetPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Datagridview = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.Addresstextbox = new System.Windows.Forms.TextBox();
+            this.Hiredatetextbox = new System.Windows.Forms.TextBox();
+            this.Jobtitletextbox = new System.Windows.Forms.TextBox();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Datagridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Datagridview
+            // 
+            this.Datagridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Datagridview.Location = new System.Drawing.Point(63, 193);
+            this.Datagridview.Name = "Datagridview";
+            this.Datagridview.Size = new System.Drawing.Size(419, 166);
+            this.Datagridview.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(252, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(252, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(427, 12);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(75, 23);
+            this.AddBtn.TabIndex = 3;
+            this.AddBtn.Text = "Add ";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // Addresstextbox
+            // 
+            this.Addresstextbox.Location = new System.Drawing.Point(252, 67);
+            this.Addresstextbox.Name = "Addresstextbox";
+            this.Addresstextbox.Size = new System.Drawing.Size(117, 20);
+            this.Addresstextbox.TabIndex = 4;
+            // 
+            // Hiredatetextbox
+            // 
+            this.Hiredatetextbox.Location = new System.Drawing.Point(252, 93);
+            this.Hiredatetextbox.Name = "Hiredatetextbox";
+            this.Hiredatetextbox.Size = new System.Drawing.Size(117, 20);
+            this.Hiredatetextbox.TabIndex = 5;
+            // 
+            // Jobtitletextbox
+            // 
+            this.Jobtitletextbox.Location = new System.Drawing.Point(252, 119);
+            this.Jobtitletextbox.Name = "Jobtitletextbox";
+            this.Jobtitletextbox.Size = new System.Drawing.Size(117, 20);
+            this.Jobtitletextbox.TabIndex = 6;
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(427, 91);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(75, 23);
+            this.SaveBtn.TabIndex = 7;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Firstname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Lastname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(195, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Hiredate";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(200, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Jobtitle";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(69, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Employee Table";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 367);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.Jobtitletextbox);
+            this.Controls.Add(this.Hiredatetextbox);
+            this.Controls.Add(this.Addresstextbox);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Datagridview);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Datagridview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Datagridview;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.TextBox Addresstextbox;
+        private System.Windows.Forms.TextBox Hiredatetextbox;
+        private System.Windows.Forms.TextBox Jobtitletextbox;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ DatasetPractice/DatasetPractice/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DatasetPractice.DataSet1TableAdapters;
+
+namespace DatasetPractice
+{
+    public partial class Form1 : Form
+    {
+        DataSet1.EmployeesDataTable emptable = new DataSet1.EmployeesDataTable();
+        EmployeesTableAdapter empadapter = new EmployeesTableAdapter();
+
+        public Form1()
+
+        {
+            InitializeComponent();
+       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            empadapter.Fill(emptable);
+            DataTable dtable = emptable;
+            Datagridview.DataSource = emptable;           
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            DataSet1.EmployeesRow employeerow = emptable.NewEmployeesRow();
+            employeerow.emp_firstname = textBox1.Text;
+            employeerow.emp_lastname = textBox2.Text;
+            employeerow.emp_address = Addresstextbox.Text;
+            employeerow.emp_hiredate = Hiredatetextbox.Text;
+            employeerow.emp_jobtitle = Jobtitletextbox.Text;
+            emptable.AddEmployeesRow(employeerow);
+            Datagridview.Refresh();
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            empadapter.Update(emptable);
+        }
+    }
+}
